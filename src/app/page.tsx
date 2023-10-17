6c6b7eb18382e36f29b5d41dea9314b92e13f016
joeltonluz <joelton.luz@bughunt.com.br>
2023-10-16 22:50:34 -0300

    fix: change data user

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,5 +5,5 @@ import { useSession } from "next-auth/react";
 export default function Home() {
   const { data } = useSession();
 
-  return <div>Bem Vindo {data?.user?.name}</div>;
+  return <div> {`Bem-vindo: ${data?.user?.name}`}</div>;
 }
