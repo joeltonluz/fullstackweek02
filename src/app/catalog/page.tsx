658b6689150f3ba07ad42bc08023b5753f1c8f3f
joeltonluz <joelton.luz@bughunt.com.br>
2023-10-18 19:18:46 -0300

    feat: add page to category show

--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -1,21 +1,21 @@
 import { Badge } from "@/components/ui/badge";
 import { prismaClient } from "@/lib/prisma";
 import { ShapesIcon } from "lucide-react";
-import Categories from "../(home)/components/categories";
 import CategoryItem from "./components/category-item";
 
 const CatalogPage = async () => {
-  const categories = await prismaClient.category.findMany();
+  const categories = await prismaClient.category.findMany({});
 
   return (
     <div className="flex flex-col gap-8 p-5">
       <Badge
         className="w-fit gap-1 border-2 border-primary px-3 py-[0.375rem] text-base uppercase"
-        variant={"outline"}
+        variant="outline"
       >
         <ShapesIcon size={16} />
         Catálogo
       </Badge>
+
       <div className="grid grid-cols-2 gap-8">
         {categories.map((category) => (
           <CategoryItem key={category.id} category={category} />
