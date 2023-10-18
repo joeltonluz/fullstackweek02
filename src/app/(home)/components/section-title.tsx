ea7dfc9055879819f16ae9aee1394033f1906280
joeltonluz <joelton.luz@bughunt.com.br>
2023-10-18 17:14:22 -0300

    fix: include padding-left at component

--- src/app/(home)/components/section-title.tsx
+++ src/app/(home)/components/section-title.tsx
@@ -2,7 +2,7 @@ import { ComponentProps } from "react";
 
 const SectionTitle = ({ children, ...props }: ComponentProps<"p">) => {
   return (
-    <p className="mb-3 pl35 font-bold uppercase" {...props}>
+    <p className="mb-3 pl-4 font-bold uppercase" {...props}>
       {children}
     </p>
   );
