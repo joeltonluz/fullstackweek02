f884c988ab517573097f36638b081bab7d33fb29
joeltonluz <joelton.luz@bughunt.com.br>
2023-10-18 15:52:40 -0300

    feat: add Category and CategoryItem components

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,18 +2,24 @@
 
 import { useSession } from "next-auth/react";
 import Image from "next/image";
+import Categories from "./components/categories";
 
 export default function Home() {
   const { data } = useSession();
 
   return (
-    <Image
-      src="/banner-home-01.png"
-      height={0}
-      width={0}
-      className="h-auto w-full"
-      sizes="100vw"
-      alt="Até 55% de desconto esse mês!"
-    />
+    <div className="p-5">
+      <Image
+        src="/banner-home-01.png"
+        height={0}
+        width={0}
+        className="h-auto w-full"
+        sizes="100vw"
+        alt="Até 55% de desconto esse mês!"
+      />
+      <div className="mt-32">
+        <Categories />
+      </div>
+    </div>
   );
 }
