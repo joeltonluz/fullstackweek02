b5639f1497b2dd63876894105cca90380693b8ee
joeltonluz <joelton.luz@bughunt.com.br>
2023-10-19 20:46:45 -0300

    feat: ensure recommends products are shown on product details page

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className="bg-accent px-8 py-4 text-[0.625rem] opacity-75">
+    <div className="bg-primary-foreground px-8 py-4 text-[0.625rem] opacity-75">
       2023 Copyright <span className="font-semibold">NEXT Store</span>
     </div>
   );
