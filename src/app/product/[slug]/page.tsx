e23dcd16ee06ef7c298183caaefa2d6fd1bc6b76
joeltonluz <joelton.luz@bughunt.com.br>
2023-10-19 22:53:03 -0300

    refactor: include components to product/slug

--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -1,6 +1,6 @@
 import { prismaClient } from "@/lib/prisma";
-import ProductImages from "../components/product-images";
-import ProductInfo from "../components/product-info";
+import ProductImages from "./components/product-images";
+import ProductInfo from "./components/product-info";
 import { computeProductTotalPrice } from "@/helpers/product";
 import ProductList from "@/components/ui/product-list";
 import SectionTitle from "@/components/ui/section-title";
